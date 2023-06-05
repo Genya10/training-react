5e9487cf08a57159b9ebdab980e428910602f75c
Jenya <113167825+Genya10@users.noreply.github.com>
2023-06-05 18:32:39 +0300

    add file header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header=()=>{
+    return(
+        <div>
+            <div>Hello,everybody</div>
+            <img src="https://m.media-amazon.com/images/M/MV5BYjhiNjBlODctY2ZiOC00YjVlLWFlNzAtNTVhNzM1YjI1NzMxXkEyXkFqcGdeQXVyMjQxNTE1MDA@._V1_.jpg"/>
+        </div>
+    )
+}
+
+export default Header;
